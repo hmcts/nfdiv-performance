99cfcdc1b8de1908778cafb9286c304aa6b5bbbe
Jon Soloway <jon.soloway@hmcts.net>
2021-05-20 15:37:40 +0100

    Updating simulation config

--- src/gatling/simulations/simulations/NFD_Pipeline.scala
+++ src/gatling/simulations/simulations/NFD_Pipeline.scala
@@ -4,6 +4,7 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.controller.inject.open.OpenInjectionStep
+import io.gatling.commons.stats.assertion.Assertion
 import scenarios._
 import utils.Environment
 import scala.concurrent.duration._
@@ -15,7 +16,13 @@ class NFD_Pipeline extends Simulation {
   /* TEST TYPE DEFINITION */
   /* perftest = performance test against the perftest environment */
   /* pipeline = nightly pipeline against the AAT environment  */
-  val testType = System.getProperty("TEST_TYPE")
+  val testType = System.getProperty("TEST_TYPE", "")
+
+  val env = testType match{
+    case "perftest" => "perftest"
+    case "pipeline" => "aat"
+    case _ => "**INVALID**"
+  }
 
   /* ******************************** */
 
@@ -28,7 +35,7 @@ class NFD_Pipeline extends Simulation {
   /* ******************************** */
 
   /* PIPELINE CONFIGURATION */
-  val numberOfPipelineUsers:Double = 10
+  val numberOfPipelineUsers:Double = 2
   /* ******************************** */
 
   val httpProtocol = Environment.HttpProtocol
@@ -37,17 +44,24 @@ class NFD_Pipeline extends Simulation {
     .inferHtmlResources()
     .silentResources
 
+  before{
+    println(s"Test Type: ${testType}")
+    println(s"Test Environment: ${env}")
+    println(s"Test Duration: ${testDurationMins} minutes")
+  }
+
   val NFDSimulation = scenario( "NFDSimulation")
     .exitBlockOnFail {
       exec(flushHttpCache)
-        .exec(flushCookieJar)
-        .exec(
-          CreateUser.CreateCitizen,
-          Homepage.NFDHomepage,
-          Login.NFDLogin,
-          NFD_01DivorceApplication.ApplicationQuestions1,
-          NFD_02DivorceApplication.ApplicationQuestions2,
-          Logout.NFDLogout)
+      .exec(flushCookieJar)
+      .exec(_.set("env", s"${env}"))
+      .exec(
+        CreateUser.CreateCitizen,
+        Homepage.NFDHomepage,
+        Login.NFDLogin,
+        NFD_01DivorceApplication.ApplicationQuestions1,
+        NFD_02DivorceApplication.ApplicationQuestions2,
+        Logout.NFDLogout)
     }
     .exec(DeleteUser.DeleteCitizen)
 
@@ -72,9 +86,20 @@ class NFD_Pipeline extends Simulation {
     }
   }
 
+  def assertions(simulationType: String): Seq[Assertion] = {
+    simulationType match {
+      case "perftest" =>
+        Seq(global.successfulRequests.percent.gte(95))
+      case "pipeline" =>
+        Seq(global.successfulRequests.percent.is(100))
+      case _ =>
+        Seq()
+    }
+  }
+
   setUp(
     NFDSimulation.inject(simulationProfile(testType))
   ).protocols(httpProtocol)
-    .assertions(global.successfulRequests.percent.is(100))
+    .assertions(assertions(testType))
 
 }
