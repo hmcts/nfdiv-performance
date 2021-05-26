c8da0aff65ef24f520627343ad47e1d45614a3cf
Jon Soloway <jon.soloway@hmcts.net>
2021-05-26 11:10:53 +0100

    Updating pipeline assertions

--- src/gatling/simulations/simulations/NFD_Simulation.scala
+++ src/gatling/simulations/simulations/NFD_Simulation.scala
@@ -101,7 +101,8 @@ class NFD_Simulation extends Simulation {
       case "perftest" =>
         Seq(global.successfulRequests.percent.gte(95))
       case "pipeline" =>
-        Seq(global.successfulRequests.percent.is(100))
+        Seq(global.successfulRequests.percent.is(95),
+          details("DivorceApp_300_PayYourFee").successfulRequests.count.gte((numberOfPipelineUsers * 0.8).ceil.toInt))
       case _ =>
         Seq()
     }
