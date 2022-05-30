7d7b296a481f9b5b82cfe85dc0a82be4c1b3f8e0
Lara <laratollington@gmail.com>
2022-05-30 16:43:15 +0100

    Update progress bar check for co

--- src/gatling/simulations/scenarios/NFD_03_CitizenApplyForCO.scala
+++ src/gatling/simulations/scenarios/NFD_03_CitizenApplyForCO.scala
@@ -124,8 +124,8 @@ object NFD_03_CitizenApplyForCO {
         .formParam("_csrf", "${csrf}")
         .multivaluedFormParam(session => "co" + session("userType").as[String].replace("applicant", "Applicant") + "StatementOfTruth", List("", Case.Checkbox.Checked))
         .check(regex("You have applied for a `?conditional order`?|The court will check your application and send it to a judge"))
-        //check for completed sections (1 for joint, 3 for sole)
-        .check(substring("progress-bar__icon--complete").count.in(1, 3)))
+        //check for completed sections (1 - joint with 1 app applied, 2 - joint both apps applied, 3 for sole)
+        .check(substring("progress-bar__icon--complete").count.in(1, 2, 3)))
 
     }
 
