400c7763a7d1ee099d0b2879f2e6ec1fb169f45c
Jon Soloway <jon.soloway@hmcts.net>
2025-05-16 13:50:18 +0100

    Updates to model

--- src/gatling/simulations/simulations/Create_NFD_Cases.scala
+++ src/gatling/simulations/simulations/Create_NFD_Cases.scala
@@ -25,58 +25,56 @@ class Create_NFD_Cases extends Simulation {
     case _ => disabledPauses
   }
 
-  val UserFeeder = csv("UserData.csv").circular
+  val UserFeeder = csv("UserData.csv").random
 
-  val users = 2
-  val numberOfCasesToCreatePerUser = 10
+  val users = 4
+  val numberOfCasesToCreatePerUser = 3
 
   val CreateNFDCase = scenario( "CreateNFDCase")
     .exec(_.set("env", s"${env}"))
+    //feed 2 users at a time (one applicant solicitor, one respondent solicitor)
+    .feed(UserFeeder, 2)
     .repeat(if (debugMode == "off") numberOfCasesToCreatePerUser else 1){
 
-      feed(UserFeeder, 2)
+      exitBlockOnFail {
+        //initialise gatling session variables
+        exec { session =>
+          session.set("user1", session("user").as[Array[AnyRef]].toSeq.apply(0))
+            .set("user2", session("user").as[Array[AnyRef]].toSeq.apply(1))
+            .set("password1", session("password").as[Array[AnyRef]].toSeq.apply(0))
 
-      .exec { session =>
-        session.set("user1", session("user").as[Array[AnyRef]].toSeq.apply(0))
-          .set("user2", session("user").as[Array[AnyRef]].toSeq.apply(1))
-          .set("password1", session("password").as[Array[AnyRef]].toSeq.apply(0))
+            .set("orgname1", session("orgname").as[Array[AnyRef]].toSeq.apply(0))
+            .set("orgname2", session("orgname").as[Array[AnyRef]].toSeq.apply(1))
 
-          .set("orgname1", session("orgname").as[Array[AnyRef]].toSeq.apply(0))
-          .set("orgname2", session("orgname").as[Array[AnyRef]].toSeq.apply(1))
+            .set("orgref1", session("orgref").as[Array[AnyRef]].toSeq.apply(0))
+            .set("orgref2", session("orgref").as[Array[AnyRef]].toSeq.apply(1))
 
-          .set("orgref1", session("orgref").as[Array[AnyRef]].toSeq.apply(0))
-          .set("orgref2", session("orgref").as[Array[AnyRef]].toSeq.apply(1))
+            .set("applicant1FirstName", ("App1" + Common.randomString(5)))
+            .set("applicant1LastName", ("Test" + Common.randomString(5)))
+            .set("applicant2FirstName", ("App2" + Common.randomString(5)))
+            .set("applicant2LastName", ("Test" + Common.randomString(5)))
 
-          .set("applicant1FirstName", ("App1" + Common.randomString(5)))
-          .set("applicant1LastName", ("Test" + Common.randomString(5)))
-          .set("applicant2FirstName", ("App2" + Common.randomString(5)))
-          .set("applicant2LastName", ("Test" + Common.randomString(5)))
+            .set("marriageDate",
+              DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 10, maxYears = 50, minMonths = 0, maxMonths = 11, minDays = 0, maxDays = 30))
+        }
 
-          .set("marriageDate",
-            DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 10, maxYears = 50, minMonths = 0, maxMonths = 11, minDays = 0, maxDays = 30))
-      }
+        //first upload a document
+        .exec(CcdHelper.uploadDocumentToCdam(
+          userEmail = "#{user1}",
+          userPassword = "#{password1}",
+          caseType = CcdCaseTypes.DIVORCE_NFD,
+          filepath = "testFile.pdf"
+        ))
 
-      //first upload a document
-      .exec(CcdHelper.uploadDocumentToCdam(
-        userEmail = "#{user1}",
-        userPassword = "#{password1}",
-        caseType = CcdCaseTypes.DIVORCE_NFD,
-        filepath = "testFile.pdf"
-      ))
+        //create a case
+        .exec(CcdHelper.createCase(
+          "#{user1}",
+          "#{password1}",
+          CcdCaseTypes.DIVORCE_NFD,
+          "solicitor-create-application",
+          "createCase.json"
+        ))
 
-      //create a case
-      .exec(CcdHelper.createCase(
-        "#{user1}",
-        "#{password1}",
-        CcdCaseTypes.DIVORCE_NFD,
-        "solicitor-create-application",
-        "createCase.json"
-      ))
-
-      .exec {
-        session =>
-          println(session)
-          session
       }
 
     }
