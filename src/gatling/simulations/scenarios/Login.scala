eeef4ce47ff63f49ab65149271d7cd73ca3f0509
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2026-09-09 09:15:31 +0100

    NFDIV-5763 Fix performance pipeline for new Idam login

--- src/gatling/simulations/scenarios/Login.scala
+++ src/gatling/simulations/scenarios/Login.scala
@@ -6,6 +6,9 @@ import utils.{CsrfCheck, Environment}
 
 import scala.concurrent.duration._
 
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 object Login {
 
   val BaseURL = Environment.baseURL
@@ -20,8 +23,9 @@ object Login {
   //userType: Applicant1 or Applicant2
   //redirectURLSuffix: callback or callback-applicant2 (use callback-applicant2 if the user is landing on /login-applicant2 or /respondent)
   //nextPageTextCheck: unique text that appears on the subsequent page for validation
-  def NFDLogin(userType: String, redirectURLSuffix: String, nextPageTextCheck: String) =
-
+  def NFDLogin(userType: String, redirectURLSuffix: String, nextPageTextCheck: String) = {
+    val redirectUri = s"$BaseURL/oauth2/$redirectURLSuffix"
+    val encodedRedirectUri = URLEncoder.encode(redirectUri, StandardCharsets.UTF_8.toString)
     exec {
       session =>
         session
@@ -30,17 +34,36 @@ object Login {
     }
 
     .group("NFD_000_Login") {
-        exec(http("Login Applicant1")
-          .post(IdamURL + "/login?client_id=divorce&response_type=code&redirect_uri=" + BaseURL + "/oauth2/" + redirectURLSuffix)
+        // Step 1: load IDAM sign-in landing page
+        exec(http("Login Applicant1 - Load Sign In")
+            .get(s"$IdamURL/o/authorize?client_id=nfdiv&response_type=code&redirect_uri=$encodedRedirectUri&scope=openid%20profile%20roles")
+            .headers(CommonHeader)
+            .check(status.in(200, 302, 303))
+            .check(CsrfCheck.save)
+            .check(substring("Sign in or create an account"))
+        )
+        // Step 2: submit email page
+        .exec(http("Login Applicant1 - Enter Email")
+           .post(IdamURL + "/enter-email")
+           .headers(CommonHeader)
+           .headers(PostHeader)
+           .formParam("email", "#{emailAddress}")
+           .formParam("_csrf", "#{csrf}")
+           .check(status.in(200, 302, 303))
+           .check(CsrfCheck.save)
+           .check(substring("Enter your password"))
+        )
+        // 3) Submit password
+        .exec(http("Login Applicant1 - Enter Password")
+          .post(IdamURL + "/enter-password")
           .headers(CommonHeader)
           .headers(PostHeader)
-          .formParam("username", "#{emailAddress}")
           .formParam("password", "#{password}")
-          .formParam("save", "Sign in")
-          .formParam("selfRegistrationEnabled", "true")
           .formParam("_csrf", "#{csrf}")
-          .check(CsrfCheck.save)
-          .check(substring(nextPageTextCheck)))
+          .check(status.in(200, 302, 303))
+          .check(CsrfCheck.save.optional)
+          .check(substring(nextPageTextCheck))
+        )
     }
 
     //set session variables for subsequent calls based on the userType (these are used to drive the URLs and form parameters)
@@ -58,5 +81,6 @@ object Login {
     }
 
     .pause(MinThinkTime.seconds, MaxThinkTime.seconds)
+  }
 
 }
